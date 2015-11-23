a0f95862d5a59735b4f3575fa7dfadad7731f340
langenoir <Ln1878@langenoirs-MBP.att.net>
2015-11-22 23:00:37 -0600

    final project startups

---
+++ index.php
@@ -0,0 +1,150 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<style>
+.image-pos{
+    
+    margin-right: 123px;
+}
+.image-pos2{
+    margin-left: 10px;
+}
+</style>
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+   
+
+    <title>MirrorCo. Index</title>
+
+    <!-- Bootstrap Core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom CSS -->
+    <link href="css/thumbnail-gallery.css" rel="stylesheet">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+</head>
+
+<body>
+    <!-- Navigation -->
+    <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+        <div class="container">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="index.php">MirrorCo.</a>
+            </div>
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav">
+
+                    <li>
+                        <a href="about.php">About</a>
+                    </li>
+
+                    <li>
+                        <a href="register.php">Register</a>
+                    </li>
+
+                    <li>
+                        <a href="login.php">Sign In</a>
+                    </li>
+
+                    <li>
+                        <a href="contact.php">Contact</a>
+                    </li>
+
+                </ul>
+            </div>
+            <!-- /.navbar-collapse -->
+        </div>
+        <!-- /.container -->
+    </nav>
+
+     <!--DB connection-->
+	<?php 
+	 
+	?>
+
+    <!-- Page Content -->
+    <div class="container">
+
+        <div class="row">
+
+            <div class="col-lg-12">
+               <h1 class="page-header"> Index</h1>
+            </div>
+           
+        </div>
+        
+        <div class = "image-pos">
+        <img src="images/camera.png" height="251" width="333" align="right">
+        </div>
+    
+    
+    <h3>Project Introduction</h3>
+    <br>
+    <p>This web application is designed to bring <font color = "#bf8040"><b>warmth</b> </font>to people</p>
+    <p>You can teach the kittens to talk, they will remember all the words you said</p>
+    <p>And <b><font color="#bf8040">one day</font></b>, all of a sudden</p>
+    <p>They will talk the right words to you </p>
+    <p>When you just need courage and</p>
+    <p><font color = "#bf8040"><b>Love</b>...</font></p>
+    <br>
+    <p>We are all mirrors to each other in this world</p>
+    <br>
+    <h1 align="center"><font color = "grey">Stop Cyber Bullying & Start The Warmest Miracles</font></h1>
+    <br><br><br><br>
+
+        <div class = "image-pos2">
+        <img src="images/purr.png"  align="left">
+        </div>
+    
+        <h3>Testing Instruction</h3>
+        <p>Please register or use the default login username and password to the controllor.</p>
+        <p><b>Default Username:</b> Yiming </p>
+        <p><b>Default Password:</b> ZHANG <font color = "grey"> - Case Sensitive</font></p>
+        <br>
+        <p><a href="about.php"><font color = "#bf8040"><b><u>Read more...</u></b></font></a></p>
+        <br><br><br><br>
+   
+
+
+        <hr>
+
+       
+    <!-- /.container -->
+
+    <!-- jQuery -->
+    <script src="js/jquery.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="js/bootstrap.min.js"></script>
+
+</body>
+ <!-- Footer -->
+        <footer>
+            <div class="row">
+                <div class="col-lg-12" align="center">
+                    <p>Copyright &copy; Yiming ZHANG ITMD 562 FP 15 Fall</p>
+                </div>
+            </div>
+        </footer>
+
+    </div>
+</html>
